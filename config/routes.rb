baf8080899d87b78a374613e4dc3d8ae8bbcbc51
gsaenz <gsaenz@territoriumlife.com>
2015-05-13 00:13:42 -0500

    Namespace para el app, con subdominio api

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
 
-
+  namespace :api, defaults: { format: :json },
+                              constraints: { subdomain: 'api' }, path: '/'  do
+    # We are going to list our resources here
+    
+  end
 end
