f5bf4e784aa518ab8dfd35fb0dcda9cc87270ed6
gsaenz <gsaenz@territoriumlife.com>
2015-05-16 02:13:12 -0500

    Mistake

--- spec/controllers/api/v1/tags_controller_spec.rb
+++ spec/controllers/api/v1/tags_controller_spec.rb
@@ -1,19 +1,29 @@
 require 'spec_helper'
 
 describe Api::V1::TagsController do
+    before(:each){
+    request.headers['Accept'] = "application/vnd.instahash.com+json; version=1"
+  }
 
-  describe "GET 'create'" do
-    it "returns http success" do
-      get 'create'
-      response.should be_success
+   describe "GET #create" do
+    before(:each) do 
+      get :create, format: :json, photo_id: "10150836244595317", tag:"#Alalala" 
     end
-  end
 
-  describe "GET 'destroy'" do
-    it "returns http success" do
-      get 'destroy'
-      response.should be_success
+    it "responds with 201" do 
+        expect(response.status).to eql 201
     end
-  end
+
+    it "responds with JSON" do
+      expect(response.content_type).to eql Mime::JSON.to_s
+    end
+
+    it "returns a tag object in json" do 
+        tag_response = JSON.parse(response.body, symbolize_names: true)
+        expect(tag_response[:tag]).to have_key(:photo_id)
+      end
 
 end
+
+end
+
